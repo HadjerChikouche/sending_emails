1e95b46cc3e4f80b2e509f0a4e4eccef12a1622f
Amine <aminx4@gmail.com>
2018-03-19 14:31:27 +0100

    feat(sendin-email-with-attachement) : sending an email with an image.

--- send-email.py
+++ send-email.py
@@ -1,15 +1,29 @@
+import os
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-msg = "\r\n".join([
-  "From: piege.photographique1@gmail.com",
-  "To: chikouche.hadjer@gmail.com",
-  "Subject: Just a message",
-  "",
-  "ERWAN le BAWSr!!"
-  ])
-server.login("piege.photographique1@gmail.com", "azertyuiop1")
-server.sendmail("piege.photographique1@gmail.com", "chikouche.hadjer@gmail.com", msg)
-server.quit()
+imgFileName = 'image.png'
+server = 'smtp.gmail.com'
+port = 587
+img_data = open(imgFileName, 'rb').read()
+msg = MIMEMultipart()
+msg['Subject'] = 'Image'
+msg['From'] = 'piege.photographique1@gmail.com'
+msg['To'] = 'chikouche.hadjer@gmail.com'
+
+text = MIMEText("This is My Image ... :) ")
+msg.attach(text)
+image = MIMEImage(img_data, name=os.path.basename(imgFileName))
+msg.attach(image)
+
+
+s = smtplib.SMTP(server, port)
+s.ehlo()
+s.starttls()
+s.ehlo()
+
+s.login('piege.photographique1@gmail.com', 'azertyuiop1')
+s.sendmail('piege.photographique1@gmail.com', 'chikouche.hadjer@gmail.com', msg.as_string())
+s.quit()
